29315fa86e00fde1588e374ace254b690a732056
ziying <zi_ying123@163.com>
2020-11-02 10:35:43 +0800

    'req'

--- common/basic/req_func.py
+++ common/basic/req_func.py
@@ -7,6 +7,7 @@ __function__ = 'xxx'
 """
 
 import requests
+import json
 
 
 class RequestHandler:
@@ -25,19 +26,39 @@ class RequestHandler:
 if __name__ == '__main__':
     # 以下是测试代码
 
-    # url = 'https://www.huya.com/g/lol'
-    url = 'https://udblgn.huya.com/web/v2/passwordLogin'
+    url = 'http://mp-ttsx-python.itheima.net/user/login'
+    url1 = 'https://api.github.com/'
 
     payload = {
         "username": "",
         "password": ""
     }
 
+    headers = {
+        "Content-Type": "application/json; charset=utf-8",
+        'Connection': 'keep-alive'
+    }
+
     req = RequestHandler()
-    res = req.visit("post", url, json=payload)
-    print(res.json())
-    # res = req.visit("get", url)
+    res = req.visit("post", url, data=json.dumps(payload), headers=headers)
 
-    print(res.status_code)
-    print(res.reason)
+    try:
+        print(res.status_code, res.reason)
+        if res.status_code != 200 or res.reason != "OK":
+            raise Exception
+        st = res.json()
+        print(st)
+    except Exception as e:
+        print(e)
+        raise
 
+    res1 = req.visit("get", url1, headers=headers)
+
+    try:
+        print(res1.status_code, res1.reason)
+        if res1.status_code == 200 and res1.reason == "OK":
+            st = res1.json()
+            print(st)
+    except Exception as e:
+        print(e)
+        raise
